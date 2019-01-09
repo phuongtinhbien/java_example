e22cf69ec2b596d61ec7a8303d23eaae08cdc30b
phuongtinhbien <phuongtinhbien12@gmail.com>
2019-01-09 14:33:13 +0700

    innit 2

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,13 @@ public class Main {
     public static void main(String[] args) {
         int chieurong;
         int chieudai;
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Nhap chieu rong:");
         chieurong = scanner.nextInt();
         System.out.println("Nhap chieu dai:");
         chieudai = scanner.nextInt();
-
         //------------------
-
         shape a = new shape(chieudai, chieurong);
-
         System.out.println("dien tich: "+a.dientich());
 
 
